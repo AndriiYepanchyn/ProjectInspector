f7ca4d996b2ac34911a8a129400dc8c3e0f324ce
Yepanchyn <ayepanchyn@luxoft.com>
2025-05-28 22:25:55 +0300

    Add echo message in run-local-fx

--- run-local-fx.bat
+++ run-local-fx.bat
@@ -1,3 +1,3 @@
 
-
+ECHO Ensure you've set env variable JAVAFX_PATH=%JAVAFX_PATH%
 java --module-path %JAVAFX_PATH% --add-modules javafx.controls,javafx.fxml -jar target/projectinspector-1.0-SNAPSHOT.jar
