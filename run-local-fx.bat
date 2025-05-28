fddfb2d2a89794cd22a022ee7aa049b3ed7bfb79
Yepanchyn <ayepanchyn@luxoft.com>
2025-05-29 00:09:16 +0300

    Prepare for GitHub Realeases

--- run-local-fx.bat
+++ run-local-fx.bat
@@ -1,3 +1,3 @@
 
-ECHO Ensure you've set env variable JAVAFX_PATH=%JAVAFX_PATH%
+ECHO Ensure you've set env variable JAVAFX_PATH (21 version required) : %JAVAFX_PATH%
 java --module-path %JAVAFX_PATH% --add-modules javafx.controls,javafx.fxml -jar target/projectinspector-1.0-SNAPSHOT.jar
