421d25459b9cfd4fea3c67faf90c0e2d48372016
AYepanchyn <AYepanchyn@AYEPANCHYN-NB>
2025-08-04 00:15:45 +0300

    Add_OpenFiles_Action

--- src/main/java/com/projectinspector/desktop/ui/actions/OpenFilesAction.java
+++ src/main/java/com/projectinspector/desktop/ui/actions/OpenFilesAction.java
@@ -3,6 +3,7 @@ package com.projectinspector.desktop.ui.actions;
 import java.io.File;
 import java.util.List;
 
+import com.projectinspector.datamodel.FileNode;
 import com.projectinspector.mainapp.MainApp;
 import com.projectinspector.persistence.PersistanceMgr;
 
@@ -10,6 +11,8 @@ import javafx.scene.image.ImageView;
 
 public class OpenFilesAction extends AbstractAction {
 	private static final long serialVersionUID = 1L;
+	private static FileNode fileNode = MainApp.getFileNode();
+	String[] filter = {"*.java", "*.py", "*.jar", "*.cpp"}; //FIXME create getter for supported types.
 	
 public OpenFilesAction(String text, char accelerator, String tooltip, ImageView pic) {
 	super(text, accelerator, tooltip,  pic);
@@ -17,11 +20,16 @@ public OpenFilesAction(String text, char accelerator, String tooltip, ImageView 
 
 	@Override
 	public void execute() {
-		String[] filter = {"*.java", "*.py", "*.jar", "*.cpp"};
 		
 		List<File> files = PersistanceMgr.chooseMultipleFiles(MainApp.getPrimaryStage(),  filter);
 		System.out.println("Selected files = " );
 		files.forEach(e ->{System.out.println(e.getAbsolutePath());});
+		System.out.println("=".repeat(40));
+		
+		fileNode.addChildren(files);
+		System.out.println(fileNode.printTree());
+		
+		
 		
 	}
 
